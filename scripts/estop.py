67d9f0d39741194cea6cb033aa7b76d3cd80f4a9
Jim Jing <jimjing131313@gmail.com>
2017-06-27 22:01:37 -0400

    Add comments

--- scripts/estop.py
+++ scripts/estop.py
@@ -4,18 +4,20 @@ import evdev
 from evdev import ecodes
 
 import sys
+# Change this to point towards the right location
 sys.path.append("/home/jim/Projects/Embedded/ecosystem/smores_build/smores_reconfig/python")
 from SmoresModule.SmoresCluster import SmoresCluster
 import time
-
+# Create cluster and other required variables for the controller
 c = SmoresCluster(range(1,27))
 d = None
 repeat = 3
 
-
+# Run until killed
 while True:
     devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
     for device in devices:
+        # Change this to the right controller name
         if device.name.startswith("Jess Tech"):
             d = evdev.InputDevice(device.fn)
 
@@ -23,6 +25,8 @@ while True:
         try:
             for event in device.read_loop():
                 if event.value == 01:
+                    # When the button is lifted
+                    # Change to match the right code
                     if event.code in [288, 289, 290, 291]:
                         print "STOP!"
                         for i in xrange(repeat):
