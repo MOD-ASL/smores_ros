f9f43a1cf983bb394360ca11e43490d40c903894
jimjing <jimjing131313@gmail.com>
2017-08-31 20:43:58 -0400

    [CORE] EoD commit

--- nodes/mission_planner_node.py
+++ nodes/mission_planner_node.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import rospy
-from smores_ros.mission_planner_stamp import MissionPlanner
+from smores_ros.mission_planner_drawer import MissionPlanner
 
 rospy.init_node('SMORES_Mission_Planner', anonymous=True, log_level=rospy.DEBUG)
 MP = MissionPlanner()
